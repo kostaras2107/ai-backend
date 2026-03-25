e15f51fb57b420e9a20121319c29d0e10b70cf51
Κώστας Μποσινάκος <bosinakos@THEODMN.GR>
2026-03-25 17:43:10 +0200

    REMOVE API

---
+++ utils.py
@@ -0,0 +1,56 @@
+import unicodedata
+import request
+import os
+
+def full_conversation(history):
+    texts = []
+    for msg in history:
+        if msg.get("isUser") and msg.get("text"):
+            texts.append(msg.get("text"))
+    return " ".join(texts)
+
+
+def get_last_user_text(history):
+    for msg in reversed(history):
+        if msg.get("isUser") and msg.get("text"):
+            return msg.get("text")
+    return ""
+
+
+def normalize_text(text):
+    if not text:
+        return ""
+    text = str(text).lower()
+    text = unicodedata.normalize("NFD", text)
+    text = text.encode("ascii", "ignore").decode("utf-8")
+    return text
+
+def web_search_context(query):
+
+    url = "https://google.serper.dev/search"
+
+    payload = {
+        "q": query,
+        "num": 10
+    }
+
+    headers = {
+        "X-API-KEY": os.getenv("SERPER_API_KEY"),
+        "Content-Type": "application/json"
+    }
+
+    try:
+        r = requests.post(url, json=payload, headers=headers)
+        print("SERPER STATUS:", r.status_code, flush=True)
+        print("SERPER RESPONSE:", r.text[:500], flush=True)
+        data = r.json()
+
+        snippets = []
+
+        for result in data.get("organic", [])[:10]:
+            snippets.append(result.get("snippet", ""))
+
+        return "\n".join(snippets)
+
+    except:
+        return ""
