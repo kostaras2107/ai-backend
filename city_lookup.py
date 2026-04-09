62afb9f47801b9825722b5b0c5ba55360f12ba52
Κώστας Μποσινάκος <bosinakos@THEODMN.GR>
2026-04-09 22:22:50 +0200

    REMOVE API

--- city_lookup.py
+++ city_lookup.py
@@ -1,17 +1,13 @@
+import os
 import pandas as pd
 
-# load once (όταν ξεκινάει το server)
-import os
-
 BASE_DIR = os.path.dirname(__file__)
-FILE_PATH = os.path.join(BASE_DIR, "city_index.csv")
+FILE_PATH = os.path.join(BASE_DIR, "data", "city_index.csv")
 
 city_df = pd.read_csv(FILE_PATH)
 
-# normalize για matching
 city_df["city"] = city_df["city"].str.lower().str.strip()
 
-# convert to dict για ταχύτητα
 CITY_MAP = dict(zip(city_df["city"], city_df["city_id"]))
 
 
@@ -20,5 +16,4 @@ def get_city_id(city_name):
         return None
 
     city_name = city_name.lower().strip()
-
     return CITY_MAP.get(city_name)
