810763b9a4007d68cf1e1c2bca518effdc230d28
Κώστας Μποσινάκος <bosinakos@THEODMN.GR>
2026-04-09 22:29:25 +0200

    REMOVE API

--- city_lookup.py
+++ city_lookup.py
@@ -2,14 +2,13 @@ import os
 import pandas as pd
 
 BASE_DIR = os.path.dirname(__file__)
-FILE_PATH = os.path.join(BASE_DIR, "data", "city_index.csv")
+FILE_PATH = os.path.join(BASE_DIR, "city_index.csv")
 
 city_df = pd.read_csv(FILE_PATH)
 
 city_df["city"] = city_df["city"].str.lower().str.strip()
 
 CITY_MAP = dict(zip(city_df["city"], city_df["city_id"]))
-
 
 def get_city_id(city_name):
     if not city_name:
